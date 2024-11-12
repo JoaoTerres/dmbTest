3fecf66f4ec253730fe2717195a1df9d5283a5c7
João Gustavo Terres <jterres@brdom02.com>
2024-11-12 13:24:06 -0300

    feat: create and configure context

---
+++ Dbm/dbm.Api/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using dbm.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dbm.Api.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+           
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
--- Dbm/dbm.Api/Program.cs
+++ Dbm/dbm.Api/Program.cs
@@ -1,15 +1,24 @@
+using dbm.Api.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+
+#region Context
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseNpgsql(connectionString);
+});
+#endregion
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
